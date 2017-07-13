c432a951bcca1dc1c80536205823ff8c9b43fd94
Devin Starks <DevStarks@gmail.com>
2017-07-12 23:14:31 -0400

    devise layout for all auth pages

--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -1,7 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  layout 'login'
-  
-  # before_action :configure_sign_in_params, only: [:create]
+    # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
