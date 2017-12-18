883384bac7c73a8b0c2ba6da5efb5dd146e43191
Devin Starks <DevStarks@gmail.com>
2017-12-18 01:05:10 -0500

    react setup

---
+++ app/assets/javascripts/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = function () {
+  return (
+    <h1>Hello</h1>
+  );
+};
+
+
+export default App;
--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,5 +1,7 @@
-var React = require('react');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
 document.addEventListener('DOMContentLoaded', function() {
-  // do stuff
+  ReactDOM.render(<App />, document.getElementById('app'));
 });
