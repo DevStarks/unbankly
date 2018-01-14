633c3dbe3699439d568a5296d16ff6422c430133
Devin Starks <DevStarks@gmail.com>
2018-01-13 20:17:55 -0500

    tests User::from_omniauth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 RSpec.describe User do
 
   it { is_expected.to respond_to(:first_name) }
@@ -40,6 +42,31 @@ RSpec.describe User do
   describe '#creditors' do
     it 'returns correct list of creditors' do
       expect(debtor.creditors).to include(creditor)
+    end
+  end
+
+  describe '::from_omniauth' do
+    let(:provider_info) { { provider: 'facebook', uid: 1 } }
+
+    let(:auth_hash) do
+      auth_hash = mock_auth_hash(:facebook)
+      auth_hash.provider = provider_info[:provider]
+      auth_hash.uid = provider_info[:uid]
+      auth_hash
+    end
+
+    context 'when user already exists' do
+      let!(:user) { create(:user, provider_info) }
+
+      it 'should return that user' do
+        expect(User.from_omniauth(auth_hash)).to eq(user)
+      end
+    end
+
+    context 'when user doesnt already exist' do
+      it 'should create that user' do
+        expect{ User.from_omniauth(auth_hash) }.to change(User, :count).by(1)
+      end
     end
   end
 end
